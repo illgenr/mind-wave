2a98402c691525305ff9c0b6261e19e3a0fac7b6
carlos <huaixian.huang@gmail.com>
2023-11-30 16:16:58 +0800

    feat: gpt3.5 is not good enough

--- mind_wave.py
+++ mind_wave.py
@@ -114,7 +114,7 @@ class MindWave:
     @catch_exception
     def send_completion_request(self, messages):
         response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
+            model = "gpt-4-1106-preview",
             messages = messages)
 
         result = ''
@@ -126,7 +126,7 @@ class MindWave:
     @catch_exception
     def send_stream_request(self, messages, callback):
         response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
+            model = "gpt-4-1106-preview",
             messages = messages,
             temperature=0,
             stream=True)
@@ -327,7 +327,7 @@ class MindWave:
 
         try:
             response = openai.ChatCompletion.create(
-                model = "gpt-3.5-turbo",
+                model = "gpt-4-1106-preview",
                 messages = messages,
                 temperature=0,
                 stream=True)
